dcc2b45f708a89debef43b97a4d56218aad806bc
Daniel Ruben de Brito <danieldebrito@outlook.com>
2018-06-22 19:38:03 -0300

    ajax - jquery
    ajax -  traer e insertar html - get y post con servidor harcodeado

---
+++ 310-ajax 02-get-post jquery/js/script.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function() {
+
+	$("#login").submit(function(){
+
+		//armo el paquete para enviar al servidor
+        
+        /*var datosFormulario = {
+			// traigo info del dom y guardo en la variables
+			usuario:$("#usuario").val(),
+			contra:$("#contra").val()
+        }*/
+		var datosFormulario = $(this).serialize();  // para simplificar, si el form tiene 50 campos
+												    // lo serializa y ahorra tipear codigo
+
+		$.get("login.php",datosFormulario,procesarDatos);
+
+		return false; //para que no recargue
+	});	
+
+	//datos deviueltos 
+	function procesarDatos(datos_devueltos){
+		if(datos_devueltos=="autorizado"){
+			$("#contenidos_externos").html("<P>usuario correcto</p>");
+		}
+		else{
+			$("#contenidos_externos").html("<P>usuario incorrecto</p>");
+		}
+	}
+}); 
